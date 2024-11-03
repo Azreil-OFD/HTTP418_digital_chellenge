1ab9d69598776d98818193acc7913e8696994c86
Azreil-OFD <fantom2413@gmail.com>
2024-11-03 21:53:23 +0500

    add Readme.md

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -14,8 +14,8 @@ nextTick(async() => {
 <template>
   <div>
     <h1>Nuxt Routing set up successfully!</h1>
-    <p>Current route: {{ route.path }}</p>
-    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
-    {{ message }}
+    <div>
+      {{ message }}
+    </div>
   </div>
 </template>
