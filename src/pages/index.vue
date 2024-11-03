0d5b51ae9b075e6cf049a7d451646b93d0a2c7f7
Azreil-OFD <fantom2413@gmail.com>
2024-11-03 21:46:24 +0500

    first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const route = useRoute()
+const message = ref("")
+const getHello = async () => {
+  const hello = await (await fetch('/api')).json()
+  message.value = hello.message
+  return hello
+} 
+nextTick(async() => {
+  await getHello()
+})
+</script>
+
+<template>
+  <div>
+    <h1>Nuxt Routing set up successfully!</h1>
+    <p>Current route: {{ route.path }}</p>
+    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
+    {{ message }}
+  </div>
+</template>
