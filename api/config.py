1d710e3012d99efc624f3c88a1e323eacaa2dfe9
ysonofriychuk <ysonofriychuk@avito.ru>
2024-11-15 22:06:31 +0500

    [backend] add ctrate order contract

--- api/config.py
+++ api/config.py
@@ -1,7 +1,7 @@
-DB_USER = "backend"
-DB_PASSWORD = "FNaF1122"
-DB_NAME = "hhtp418"
-DB_HOST = "185.178.45.234"
+DB_USER = "gen_user"
+DB_PASSWORD = "A(4G34y*#LKrp~"
+DB_NAME = "default_db"
+DB_HOST = "195.133.50.12"
 DB_PORT = "5432"
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
